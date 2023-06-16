5ef9d3ab3e122b259a9f535cb45e4173d3f23e83
Jay55Martinez <martinez.jay@northeastern.edu>
2023-06-16 17:10:17 -0400

    pa12

--- activities/pa12.cpp
+++ activities/pa12.cpp
@@ -4,47 +4,47 @@
 using namespace std;
 
 class MyString {
-  char* str;
+  unique_ptr<char[]> str;
 
 public:
   MyString() : str(nullptr) { // Default Constructor
-    str = new char[1];
+    str.reset(new char[1]);
     str[0] = '\0';
   }
 
-  MyString(char* val) { // Constructor with 1 argument
+  MyString(const char* val) { // Constructor with 1 argument
     if (val == nullptr) {
-      str = new char[1];
+      str.reset(new char[1]);
       str[0] = '\0';
     } else {
-      str = new char[strlen(val) + 1];
-      strcpy(str, val);
-      str[strlen(val)] = '\0';
-      cout << "The given string is: " << str << endl;
+      size_t length = strlen(val);
+      str.reset(new char[length + 1]);
+      strcpy(str.get(), val);
+      str[length] = '\0';
+      cout<<"The given string is: "<<str.get()<<std::endl;
     }
   }
 
   MyString(const MyString& source) { // Copy Constructor
-    str = new char[strlen(source.str) + 1];
-    strcpy(str, source.str);
-    str[strlen(source.str)] = '\0';
+    size_t length = strlen(source.str.get());
+    str.reset(new char[length + 1]);
+    strcpy(str.get(), source.str.get());
   }
 
-  MyString(MyString&& source) { // Move Constructor
-    str = source.str;
-    source.str = nullptr;
+  MyString(MyString&& source) noexcept : str(nullptr) { // Move Constructor
+    str = std::move(source.str);
   }
 
-  ~MyString() { delete[] str; } // Destructor
+  ~MyString() = default; // Destructor not needed because of smart pointers
 };
 
-int main(void) {
-  cout << "Start!" << endl;
+int main() {
+  cout<<"Start!"<<endl;
 
   MyString a;
-  MyString b{(char*)"Hello"};
-  MyString c{a};
-  MyString d{MyString{(char*)"World"}};
+  MyString b("Hello");
+  MyString c(a);
+  MyString d(MyString("World"));
 
-  cout << "Bye" << endl;
+  cout<<"Bye"<<endl;
 }
